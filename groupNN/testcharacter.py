72ed0a08d28b5b9002dc68a726e336c3606caa94
Erika <essnow@wpi.edu>
2019-02-17 14:36:07 -0500

    cleaning up

--- groupNN/testcharacter.py
+++ groupNN/testcharacter.py
@@ -15,8 +15,6 @@ class TestCharacter(CharacterEntity):
     directions = []
     i = 1
     def do(self, wrld):
-        # Your code here
-
     	start = (self.dx, self.dy)
     	exit = wrld.exitcell
     	print("astar" ,start, exit)
@@ -50,130 +48,133 @@ class Node():
         y = self.pos[1]
 
         neighbors = []
-        neighbors.append(self._validate(x + 1, y, gridCells, width, end, cost))  # check for valid neighbors
+        # check for valid neighbors
+        neighbors.append(self._validate(x + 1, y, gridCells, width, end, cost))  
         neighbors.append(self._validate(x - 1, y, gridCells, width, end, cost))
         neighbors.append(self._validate(x, y + 1, gridCells, width, end, cost))
         neighbors.append(self._validate(x, y - 1, gridCells, width, end, cost))
-        neighbors.append(self._validate(x + 1, y + 1, gridCells, width, end, cost))  # check for valid neighbors
+        neighbors.append(self._validate(x + 1, y + 1, gridCells, width, end, cost))  
         neighbors.append(self._validate(x + 1, y - 1, gridCells, width, end, cost))
         neighbors.append(self._validate(x - 1, y + 1, gridCells, width, end, cost))
         neighbors.append(self._validate(x - 1, y - 1, gridCells, width, end, cost))
-        #print("neighboors" ,neighbors)
-        return neighbors  # return all neighbors that aren't obstacles
+        
+        # return all neighbors that aren't obstacles
+        return neighbors  
 
-    def _validate(self, x, y, gridCells, width, end, cost):  # check if wall
-        #print ("In Validate: ", x, y, "Width" , width, "height", gridCells.height())
+    # check if wall
+    def _validate(self, x, y, gridCells, width, end, cost):  
         a =True
         if(x >= 0 and x < gridCells.width() and y >= 0 and y <gridCells.height()):
             if (not gridCells.wall_at(x,y)):  # convert from x,y to grid cell number
                 cellCost = 0
-                #for item in cost:
-                #    if (item.x == x and item.y == y):  # if the items are in the cost map, then assign the cost value
-                #        cellCost = item.total
-                #cellCost=cost[y*width+x]
                 if (cellCost != 0):
-                    return Node(self, (x, y), self.g + cellCost, heuristic((x, y), end))  # creates new node using cost map
+                    # creates new node using cost map
+                    return Node(self, (x, y), self.g + cellCost, heuristic((x, y), end))  
                 else:
-                    return Node(self, (x, y), self.g + 1, heuristic((x, y), end))  # create new node using default
+                    # create new node using default
+                    return Node(self, (x, y), self.g + 1, heuristic((x, y), end))  
             else:
            	    return None
         else:
             return None
 
-
-def manhattan(p1, p2):  # use manhattan distance to get current distance
+"""use Manhattan distance to get current distance"""
+def manhattan(p1, p2):  
     return abs(p1[0] - p2[0]) + abs(p2[1] - p2[1])
 
-def heuristic(p1, p2):  # use Euclidean distance to get dis to goal
+"""use Euclidean distance to get dis to goal"""
+def heuristic(p1, p2):  
     x = abs(p1[0] - p2[0])
     y = abs(p1[1] - p2[1])
     return sqrt((x * x) + (y * y))
 
-
-def AStar(start, end, gridCells, width, cost):  # calculate the distance THIS IS THE MAIN FUNCTION
-
-    # setup frontier nodes using a min heap
+"""calculate the distance THIS IS THE MAIN FUNCTION"""
+def AStar(start, end, gridCells, width, cost):  
+    #set up frontier nodes using a min heap
     frontier = []
-    startNode = Node(None, start, 0, manhattan(start, end))  # parent, position, g, h
-    heapq.heappush(frontier, (startNode.f,  random.randint(1,1001)*random.randint(1,1001),  startNode))  # puts items into min heap
-    visited = {}  # create dictionary called "visited"
+    startNode = Node(None, start, 0, manhattan(start, end)) 
+    # puts items into min heap
+    heapq.heappush(frontier, (startNode.f,  random.randint(1,1001)*random.randint(1,1001),  startNode))  
+    visited = {}
+    
     while (len(frontier) > 0):  # if no solution, exit the while loop
-        #print("The heap", frontier)
-        cur = heapq.heappop(frontier)  # The current node is the shortest distance
+        # The current node is the shortest distance
+        cur = heapq.heappop(frontier) 
         cur = cur[2]
-        if (cur.pos in visited):  # don't pursue paths that have already been searched
+        # don't pursue paths that have already been searched
+        if (cur.pos in visited):  
             continue
         if (cur.pos == end):  # quit condition
             path = tracePath(start, cur, visited)  # probably trace path && return something useful
             dir = dirPath(path)
-            #print ('Directions:', dir)
             return (path, dir)
         for neighbor in cur.getNeighbors(gridCells, width, end, cost):
             i = 0
             if (neighbor != None):
                 heapq.heappush(frontier, (neighbor.f,  random.randint(1,1001)*random.randint(1,1001) , neighbor))  # put the neighbor into the min heap
             i+=1
-        visited[cur.pos] = cur  # puts current position into the dictionary of visited places
+        visited[cur.pos] = cur  
 
     print("YOU FAILED")
     return (None, None, None)
 
 
-""" Takes last node taken in from A* (Final) and the dictionary of visited nodes, and returns the path from start to end """
-
-
+"""Takes last node taken in from A* (Final) and the dictionary of visited nodes, 
+and returns the path from start to end """
 def tracePath(start, final, visited):
     path = []
     cur = final
     path.append(cur.pos)
     while (cur.pos != start):
         parent = visited.get(cur.parent.pos)
-        path.append(parent.pos)  # add to the empty list, path, the parent node, until reaching the start position/node
+        # add to the empty list, path, the parent node, until reaching the start position/node
+        path.append(parent.pos)  
         cur = cur.parent
     path.reverse()  # reverses the list so that it goes start to finish
     return path
 
-
-def dirPath(path):  # get the waypoints
+"""gets the waypoints"""
+def dirPath(path):  
     if not path:
         return
     currDir = findDir(path[0], path[1])
     nextDir = findDir(path[0], path[1])
     dir = []
     n = 0
-    for item in path:  # check if the direction of each set of consecutive nodes is different
-        # print len(path)
+    # check if the direction of each set of consecutive nodes is different
+    for item in path: 
         if (len(path) > n + 1):
             nextDir = findDir(path[n], path[n + 1])
 
             n = n + 1
             dir.append(currDir)
             currDir = nextDir
+    
     dir.append(currDir)
-    return  dir
+    return dir
 
-
-def findDir(currNode, nextNode):  # get the directions of a node and the next
+"""get the directions of a node and the next"""
+def findDir(currNode, nextNode):  
     x = currNode[0]
     y = currNode[1]
     a = nextNode[0]
     b = nextNode[1]
-    # in global directions, not local
-    if (x + 1 == a and y == b):  # right
+
+    if (x + 1 == a and y == b):  
         return (1, 0)
-    elif (x == a and y + 1 == b):  # up
+    elif (x == a and y + 1 == b):  
         return (0, 1)
-    elif (x - 1 == a and y == b):  # left
+    elif (x - 1 == a and y == b):  
         return (-1, 0)
-    elif (x == a and y - 1 == b):  # down
+    elif (x == a and y - 1 == b):  
         return (0, -1)
-    elif (x + 1 == a and y + 1 == b):  # right
+    elif (x + 1 == a and y + 1 == b):  
         return (1, 1)
-    elif (x + 1 == a and y - 1 == b):  # up
+    elif (x + 1 == a and y - 1 == b):  
         return (1, -1)
-    elif (x - 1 == a and y + 1 == b):  # left
+    elif (x - 1 == a and y + 1 == b):  
         return (-1, 1)
-    elif (x - 1 == a and y - 1 == b):  # down
+    elif (x - 1 == a and y - 1 == b):  
         return (-1, -1)
     else:
         return (0,0)
