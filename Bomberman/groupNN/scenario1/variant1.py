bf9433819cc84a332b202bde13fcf1a476f260eb
unknown <jwong3@wpi.edu>
2019-02-23 02:53:32 -0500

    Added bomb - passed variant5git add .!

--- Bomberman/groupNN/scenario1/variant1.py
+++ Bomberman/groupNN/scenario1/variant1.py
@@ -8,7 +8,7 @@ from game import Game
 
 # TODO This is your code!
 sys.path.insert(1, '../groupNN')
-from testcharacter_expectimax import TestCharacter
+from testcharacter_expectimaxBomb import TestCharacter
 
 
 # Create the game
